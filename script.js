5da9dd9c5431f4efed78402d118bb639f3fbabcf
raunak788 <raunaksingh653@gmail.com>
2026-07-04 01:21:07 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function add(name, price){
-  cart.push({name,price});
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(name + " added!");
-}
